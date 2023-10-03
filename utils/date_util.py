b52f7de699a7cb2edf0a39fa195064bc73bd3a4b
SeolDonghun <ssalssi1@gmail.com>
2023-10-03 23:36:32 +0900

    Add next and prev monday to date_util.py

--- utils/date_util.py
+++ utils/date_util.py
@@ -1,7 +1,12 @@
 import datetime
 
-def get_next_monday(dt: datetime.datetime) -> datetime.date:
+def get_next_monday(dt: datetime.datetime) -> datetime.datetime:
     # Calculate days until next Monday (1 represents Tuesday in .weekday())
     days_until_next_monday = (7 - dt.weekday()) % 7 or 7
-    next_monday_date = dt.date() + datetime.timedelta(days=days_until_next_monday)
+    next_monday_date = dt + datetime.timedelta(days=days_until_next_monday)
     return next_monday_date
+
+def get_previous_monday(dt: datetime.datetime) -> datetime.datetime:
+    while dt.weekday() != 0:
+        dt -= datetime.timedelta(days=1)
+    return dt
