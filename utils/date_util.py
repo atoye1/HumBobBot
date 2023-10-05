cbfe53090cc101879334331010f9b94b81c1c2a7
SeolDonghun <ssalssi1@gmail.com>
2023-10-05 12:39:41 +0900

    refactor date_util.py

--- utils/date_util.py
+++ utils/date_util.py
@@ -1,12 +1,9 @@
 import datetime
 
 def get_next_monday(dt: datetime.datetime) -> datetime.datetime:
-    # Calculate days until next Monday (1 represents Tuesday in .weekday())
-    days_until_next_monday = (7 - dt.weekday()) % 7 or 7
-    next_monday_date = dt + datetime.timedelta(days=days_until_next_monday)
-    return next_monday_date
+    return get_last_monday(dt) + datetime.timedelta(days=7)
 
-def get_previous_monday(dt: datetime.datetime) -> datetime.datetime:
+def get_last_monday(dt: datetime.datetime) -> datetime.datetime:
     while dt.weekday() != 0:
         dt -= datetime.timedelta(days=1)
     return dt
